3b3ac5aff38ce35142f99228527388306d3b1e6f
Datta <dmote001@gmail.com>
2024-08-09 10:48:27 +0530

    Asian employee changes

---
+++ src/test/java/gitHub_Package/GitHubClass.java
@@ -0,0 +1,11 @@
+package gitHub_Package;
+
+public class GitHubClass {
+
+    public static void main(String[] args) {
+        developBranch();
+    }
+    public static void developBranch(){
+        System.out.println("Asian employee");
+    }
+}
